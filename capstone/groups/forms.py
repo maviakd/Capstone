8621403513787496b3dca34766707c3cfcb88fec
maviakd <70297379+maviakd@users.noreply.github.com>
2021-04-01 20:23:28 -0400

    Delete capstone directory

--- capstone/groups/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django.contrib.auth.models import User, Group, ContentType
-from .models import MyGroup
-
-class GroupUserAdd(forms.ModelForm):
-	adduser = forms.BooleanField()
-	class Meta:
-		model = MyGroup
-		fields = ['adduser']
